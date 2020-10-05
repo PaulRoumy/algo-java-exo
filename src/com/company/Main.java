aaef55099e79e06b6403691916861645fbd5b6a0
Paul Roumy <paul@MacBook-Pro-de-Paul.local>
2020-10-05 16:19:10 +0200

    tableau

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+    int [] score={1,4,5,9,4,3,1};
+    float Moyenne;
+    int Valeur=0;
+    int i;
+    for( i = 0; i< score.length ;i++) {
+            Valeur=Valeur+score[i];
+
+
     }
+    Moyenne=Valeur/i+1;
+    System.out.println(Moyenne);
+}
 }
