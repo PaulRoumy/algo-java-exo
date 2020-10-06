5c4f9b1159157a381a487e22c9fdc27e6f0a34ea
Paul Roumy <paul@MacBook-Pro-de-Paul.local>
2020-10-06 14:38:33 +0200

    Enfin

--- src/com/company/TrieBinaire.java
+++ src/com/company/TrieBinaire.java
@@ -8,44 +8,38 @@ public class TrieBinaire {
     public static float LocateIndexNumber(int Tableau[],int nb){
         int index=0;
         int Min=0;
-        int Max=Tableau.length;
-        boolean find=false;
-        boolean out=false;
+        int Max=Tableau.length-1;
 
-        while (find!=true&& out!= true) {
-            index=  (Max-Min)/2;
+
+        while (Min<=Max) {
+            index=  (Min+Max)/2;
             if (nb==Tableau[index]){
-                find=true;
+                return index;
+
 
             }else if(nb<Tableau[index]){
                 Max = index -1;
-                if (Min>Max){
-                    out =true;
-                    index=-1;
-                }
+
             }else if(nb>Tableau[index]){
                 Min = index +1;
-                if (Max<Min){
-                    out = true;
-                    index=-1;
+
 
                 }
 
 
                 }
-            }
-
-
-
-        if (find == false){
-            index =-1;
-        }
-        return index;
+        return -1;
     }
+
+
+
+
+
+
 
     public static void main(String[] args) {
      int[] tableau={1,2,3,4,5,6,7,8,9};
-     int numRechercher= 22;
+     int numRechercher= 3;
      int index= (int) LocateIndexNumber(tableau,numRechercher);
      System.out.println(index);
 
